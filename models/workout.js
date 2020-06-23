ad88d7319efcfd58f74336d0840f92adb7f39bdc
Helen Darden <hgdarden@gmail.com>
2020-06-23 15:23:15 -0400

    workout.js model outline

--- models/workout.js
+++ models/workout.js
@@ -2,11 +2,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+    day: {
 
+    },
+    exercises: [{
+        type: {
+
+        },
+        name: {
+
+        },
+        weight: {
+
+        },
+        sets: {
+
+        },
+        reps: {
+
+        },
+        distance: {
+
+        },
+        duration: {
+
+        }
+    }
+    ]
 })
 
 
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
